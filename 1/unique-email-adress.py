c5fe07f7481a39fd11ae31f812b53ee3204e530d
Gabriel Dischon <gabrielgdischon@gmail.com>
2024-04-06 23:40:22 +0300

    unique_email_adress

---
+++ 1/unique-email-adress.py
@@ -0,0 +1,10 @@
+def unique_email_adress(emails:list)->list:
+    output = []
+    for email in emails:
+        local_name, Domain_name = email.split("@")
+        local_name = local_name.split("+")[0]
+        local_name = local_name.replace(".","")
+        output.append(local_name + Domain_name)
+    return len(set(output))
+
+print(unique_email_adress(["a@leetcode.com","b@leetcode.com","c@leetcode.com"]))
