aa8dbf98e1a82687989911d5800503fab2faae51
Gabriel Dischon <gabrielgdischon@gmail.com>
2024-04-13 05:20:46 +0300

    word pattern2

--- 1/word-split.py
+++ 1/word-split.py
@@ -1,6 +1,8 @@
 def word_split(pattern:str,text:str)->bool:
     temp:dict = {}
     text_list = text.split()
+    if len(pattern) != len(text_list):
+        return False
     for index in range(len(pattern)):
         if pattern[index] not in temp:
             temp[pattern[index]] = text_list[index]
